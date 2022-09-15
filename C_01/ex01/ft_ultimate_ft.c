d3fecea2d37c99e5dbfbaa1418fe0e3f67b06af0
Ribas13 <108695074+Ribas13@users.noreply.github.com>
2022-09-15 14:38:44 +0100

    Add files via upload

--- C_01/ex01/ft_ultimate_ft.c
+++ C_01/ex01/ft_ultimate_ft.c
@@ -6,13 +6,45 @@
 /*   By: diosanto <diosanto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 10:59:43 by diosanto          #+#    #+#             */
-/*   Updated: 2022/09/15 11:02:30 by diosanto         ###   ########.fr       */
+/*   Updated: 2022/09/15 14:03:18 by diosanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
-void	ft_ultimate_ft(*********a)
+void	ft_ultimate_ft(int *********a)
 {
 	*********a = 42;
 }
+
+/*
+int	main(void)
+{
+	int	*********p1;
+	int	********p2;
+	int	*******p3;
+	int	******p4;
+	int	*****p5;
+	int	****p6;
+	int	***p7;
+	int	**p8;
+	int	*p9;
+	int	n;
+
+	n = 100;
+
+	p9 = &n;
+	p8 = &p9;
+	p7 = &p8;
+	p6 = &p7;
+	p5 = &p6;
+	p4 = &p5;
+	p3 = &p4;
+	p2 = &p3;
+	p1 = &p2;
+
+	printf("%d", n);
+	ft_ultimate_ft(p1);
+	printf("%d", n);
+}*/
--- C_01/ex02/ft_swap.c
+++ C_01/ex02/ft_swap.c
@@ -6,11 +6,12 @@
 /*   By: diosanto <diosanto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 11:02:49 by diosanto          #+#    #+#             */
-/*   Updated: 2022/09/15 11:04:58 by diosanto         ###   ########.fr       */
+/*   Updated: 2022/09/15 14:24:03 by diosanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
 void	ft_swap(int *a, int *b)
 {
@@ -20,3 +21,22 @@ void	ft_swap(int *a, int *b)
 	*a = *b;
 	*b = c;
 }
+/*
+int	main(void)
+{
+	int *p1;
+	int *p2;
+	int c1;
+	int c2;
+
+	c1 = 10;
+	c2 = 20;
+
+	p1 = &c1;
+	p2 = &c2;
+
+	printf("%d, %d\n", c1, c2);
+	ft_swap(p1, p2);
+	printf("%d, %d", c1, c2);
+}
+*/
